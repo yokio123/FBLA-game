6a552d8254c54ca605ffac29fcc939f2acd864ef
yokio123 <chrishdietrich.1@gmail.com>
2024-04-12 21:53:35 +0200

    tutorial finished

--- FBLA Game/Assets/Talk.cs
+++ FBLA Game/Assets/Talk.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class NextScene : MonoBehaviour
+
+public class Talk : MonoBehaviour
 {
+    public GameObject gameObject;
+    private bool In_Area = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        In_Area = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +20,9 @@ public class NextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (In_Area == true && Input.GetKeyDown(KeyCode.Space))
         {
-            Application.Quit();
-
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("tutorial");
+           gameObject.SetActive(true);
         }
     }
 }
--- FBLA Game/Assets/start_game.cs
+++ FBLA Game/Assets/start_game.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class NextScene : MonoBehaviour
+
+public class start_game : MonoBehaviour
 {
+    private bool In_Area = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        In_Area = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +19,12 @@ public class NextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (In_Area == true && Input.GetKeyDown(KeyCode.Space))
         {
-            Application.Quit();
+            SceneManager.LoadScene("level 1");
 
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("tutorial");
-        }
+
+        
     }
 }
