d3fd3fa5cd06f686f5de713a50531573c32347f1
yokio123 <chrishdietrich.1@gmail.com>
2024-04-12 19:59:00 +0200

    s

--- FBLA Game/Assets/NextScene.cs
+++ FBLA Game/Assets/NextScene.cs
@@ -13,6 +13,11 @@ public class NextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("level 1");
